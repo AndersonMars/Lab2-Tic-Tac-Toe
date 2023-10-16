89476ea8754cb2bbfe2ce2e7bbf2ff1247482061
AndersonMars <103541627+AndersonMars@users.noreply.github.com>
2023-10-16 02:09:30 -0700

    Shared memory and player 2
    Created functions and a player 2 class to allow for memory sharing between two processes. Currently unable to move to player 2's turn, X's go forever.

--- player2.c
+++ player2.c
@@ -13,26 +13,23 @@
 #include <spawn.h>
 
 #define IPC_RESULT_ERROR (-1)
+//filename is player 1
 #define FILENAME "tic_tac_toe.c"
 
-//environ for spawn call
-extern char **environ;
-
 //functions
-int printBoard(char * gameBoard);
+char* turn(char* gameBoard);
 int checkWin(char* gameBoard);
+int printBoard(char * gameBoard);
 int get_shared_memory(char* filename, int size);
 char* attach_shared_memory(char* filename, int size);
 bool detach_shared_memory(char *block);
 bool destroy_shared_memory(char *filename);
-char* populateBoard(char* gameBoard);
-char* turn(char* gameBoard);
 
-//global variable
+//global variables
 int n;
 bool isWinner;
 
-int main (int argc, char* argv[])
+int main(int argc, char* argv[])
 {
 	//Check for too many inputs
 	if(argc > 2)
@@ -48,49 +45,19 @@ int main (int argc, char* argv[])
 	}
 	//ERROR DETECTION: CHECK FOR WRONG INPUT IN ARGV[1]
 
-	//If we've made it here, then there is only 2 arguments
-	//create an NxN board, where N is the given argument
 	n = atoi(argv[1]);
 	int boardSize = n * n;
-	char* childArguments[2] = {"./player2", argv[1]};
+	//pull the gameBoard from player1, it's in shared memory
+	char* gameBoard = attach_shared_memory(FILENAME, boardSize);
 	
-	//boolean to track if a winner has been found
-	isWinner = false;
-	
-
-	printf("Welcome to Tic-Tac-Toe!\n");
-	printf("The goal is to get %d in a row!\n", n);
-	printf("Player 1 uses x's\n");
-	printf("Player 2 uses o's\n");
-
-	//create a shared array for the gameBoard, for both processes to use
-	char * gameBoard = attach_shared_memory(FILENAME, boardSize);
-	
-	if(gameBoard == NULL)
-	{
-		printf("ERROR: Couldn't create shared memory\n");
-		return -1;
-	}
-	
-	//populate the board
-	gameBoard = populateBoard(gameBoard);
-	
-	//create a separate process that will represent player 2
-	int status;
-	int child_id;
-
-	if(0 != posix_spawn(&child_id, childArguments[0], NULL, NULL, childArguments, environ))
-	{
-		perror("spawn failed");
-		exit(1);
-	}
 
 	while(!isWinner)
 	{
-		//Take the turn and return the updated gameBoard
+		//take the turn and return the updated gameBoard
 		turn(gameBoard);
 	}
 }
+
 
 char* turn(char* gameBoard)
 {
@@ -102,14 +69,14 @@ char* turn(char* gameBoard)
 	if(winValue == 1)
 	{
 		//output to player1 that they won, and output to player2 that they lost
-		printf("Player 1 wins!\n");
+		printf("Player 2 loses!\n");
 		isWinner = true;
 		return gameBoard;
 	}
 	else if(winValue == 2)
 	{
 		//output to player 2 that they won, and output to player 1 that they lost
-		printf("Player 1 loses!\n");
+		printf("Player 2 wins!\n");
 		isWinner = true;
 		return gameBoard;
 	}
@@ -151,22 +118,7 @@ char* turn(char* gameBoard)
 		gameBoard[(row-1) + (n * (column - 1))] = 'x';
 		
 	}
-	
 
-	return gameBoard;
-
-}
-
-char* populateBoard(char* gameBoard)
-{
-	int size = n * n;
-	for(int i = 0; i < size; i += n)
-	{
-		for(int j = 0; j < n; j++)
-		{
-			gameBoard[i + j] = '-';
-		}
-	}
 	return gameBoard;
 }
 
@@ -248,10 +200,10 @@ int checkWin(char* gameBoard)
 	}
 	if(upDiagO == n) return 2;
 
+
 	//upward diagonal
 	//start at boardSize - n, and increment down by i % (n-1) == 2 is true
 	
-
 	//check for draws
 	//if all the other checks haven't returned anything, and the board is full
 	//then its a draw
